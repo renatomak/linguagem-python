c80fc546fd64476a7a75d99d90037f033e761adc
renatomak <renato.mark.silva@gmail.com>
2021-07-18 19:48:01 -0300

    versao 3 com get e set

---
+++ poo_avancado/evolucaov3.py
@@ -0,0 +1,41 @@
+class Humano:
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+        self._idade = None
+
+    def get_idade(self):
+        return self._idade
+
+    def set_idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade deve ser um número positivo!')
+        self._idade = idade
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neamderthalensis'
+        return self
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthales')
+        return ('Austrolapiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especie()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-1]
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+
+if __name__ == '__main__':
+    jose = HomoSapiens('Jose')
+    jose.set_idade(40)
+    print(f'Nome: {jose.nome} Idade: {jose.get_idade()}')
